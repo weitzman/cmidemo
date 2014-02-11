9dab6f9dfe8938a3db6ab808c12286d7a8dcd798
Moshe Weitzman <weitzman@tejasa.com>
2014-02-11 11:39:32 -0500

    Add settings.local.php to root of repo.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -657,20 +657,6 @@ ini_set('session.cookie_lifetime', 2000000);
  *
  * Keep this code block at the end of this file to take full effect.
  */
-# if (file_exists(DRUPAL_ROOT . '/' . $conf_path . '/settings.local.php')) {
-#   include DRUPAL_ROOT . '/' . $conf_path . '/settings.local.php';
-# }
-$databases['default']['default'] = array (
-  'database' => 'alice',
-  'username' => 'root',
-  'password' => '',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['install_profile'] = 'minimal';
-$config_directories['active'] = 'sites/default/files/config_JeZmqqMqX_S80W7DKVKKMT7uH8iJIeUHkuTWks8b3ZU/active';
-$config_directories['staging'] = 'sites/default/files/config_JeZmqqMqX_S80W7DKVKKMT7uH8iJIeUHkuTWks8b3ZU/staging';
-$config_directories['vcs'] = '../config_vcs';
+if (file_exists(DRUPAL_ROOT . '/' . $conf_path . '/settings.local.php')) {
+  include DRUPAL_ROOT . '/' . $conf_path . '/settings.local.php';
+}
---
+++ settings.local.php
@@ -0,0 +1,16 @@
+<?php
+
+$databases['default']['default'] = array (
+'database' => 'alice',
+'username' => 'root',
+'password' => '',
+'prefix' => '',
+'host' => 'localhost',
+'port' => '',
+'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+'driver' => 'mysql',
+);
+$settings['install_profile'] = 'minimal';
+$config_directories['active'] = 'sites/default/files/config_JeZmqqMqX_S80W7DKVKKMT7uH8iJIeUHkuTWks8b3ZU/active';
+$config_directories['staging'] = 'sites/default/files/config_JeZmqqMqX_S80W7DKVKKMT7uH8iJIeUHkuTWks8b3ZU/staging';
+$config_directories['vcs'] = '../config_vcs';
